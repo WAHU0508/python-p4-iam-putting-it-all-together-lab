17495ba21a9c6b345b2c57ac96207a7e90886cef
WAHU0508 <sharongikenye@gmail.com>
2024-10-11 13:17:53 +0300

    User model

--- server/models.py
+++ server/models.py
@@ -7,7 +7,37 @@ from config import db, bcrypt
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+    @validates('username')
+    def username(self, key, username):
+        if not username:
+            raise ValueError("Username cannot be empty")
+        existing_user = User.query.filter(User.username == username).first()
+        if existing_user:
+            raise ValueError("Name must be unique")
+        return username
+    
+    @hybrid_property
+    def password_hash(self):
+        raise Exception("Password hashes may not be viewed")
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+    
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+    
+    def __repr__(self):
+        return f'User {self.username}, ID: {self.id}'
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
